d2d914cf91e0afd52afaf2e25dddbd28d418a212
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-05-01 15:20:52 -0400

    finsihed Section 21.6.6

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { setContext } from '@apollo/client/link/context';
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
